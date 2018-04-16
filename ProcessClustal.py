a3389fa3ca8725c24efa1bcf72d05b57b5b07776
petercombs <pcombs@stanford.edu>
2018-04-16 12:21:04 -0700

    Actually do something interesting with the blocks of matches

--- ProcessClustal.py
+++ ProcessClustal.py
@@ -38,5 +38,49 @@ def process_seq_blocks(aln, set1, set2):
 
 
 if __name__ == "__main__":
-    pass
+    inserts = 0
+    insert_bps = 0
+    dels = 0
+    del_bps = 0
+    snps = 0
+    is_in_indel = False
+    indel_lens = []
+    indel_len = 0
+    for fname in glob(path.join(argv[1], '*.clu')):
+        aln = AlignIO.read(fname, 'clustal')
+        blocks = process_seq_blocks(aln, {'sec', 'mel'}, {'sim'})
+
+        ixs = {rec.id.split('_')[0] : i for i, rec in enumerate(aln)}
+        is_in_indel = False
+        for start, stop, btype in blocks:
+            if btype != 'DISJOINT': continue
+            for i in range(start, stop):
+                if aln[ixs['sim'], i] == '-':
+                    if not is_in_indel:
+                        is_in_indel = True
+                        inserts += 1
+                        indel_lens.append(indel_len)
+                        indel_len = 0
+                    insert_bps += 1
+                    indel_len += 1
+                elif ((aln[ixs['sec'], i] == '-')
+                        and (aln[ixs['mel'], i] == '-')):
+                    if not is_in_indel:
+                        is_in_indel = True
+                        indel_lens.append(indel_len)
+                        indel_len = 0
+                        dels += 1
+                    del_bps += 1
+                    indel_len += 1
+                else:
+                    snps += 1
+                    is_in_indel = False
+
+    indel_lens.append(indel_len)
+    print(('Inserts: {inserts} ({insert_bps} bps total) \n'
+           + 'Deletions: {dels} ({del_bps} bps total) \n'
+           + 'SNPs: {snps}')
+           .format(**locals()))
+    print(indel_lens)
+
 
