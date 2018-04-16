fa49056da98bd070426c5f6a37c1291f88ed3a92
petercombs <pcombs@stanford.edu>
2018-04-16 12:25:00 -0700

    Clean up the R code, make it betterE

--- DESeq.R
+++ DESeq.R
@@ -5,30 +5,38 @@ print(args)
 counts <- read.table(args[1], header=TRUE)
 samples <- read.table('DESeqDesign.txt', header=TRUE)
 
-fbfemale <- DESeqDataSetFromMatrix(countData=counts[ , 1:4], colData=samples[1:4, ], design = ~AlignsTo)
+# Female Fat Bodies
+
+fbfemale <- DESeqDataSetFromMatrix(countData=counts[ , 1:4], colData=samples[1:4, ], design = ~Replicate + AlignsTo)
 fbfemale <- DESeq(fbfemale)
-res <- results(fbfemale)
+res <- results(fbfemale, contrast=c('AlignsTo', 'ref', 'alt'))
 res$gene <- rownames(res)
 res <- res[, c(7,1,2,3,4,5,6)]
 write.table(res, paste(dirname(args[1]), '/combined/fbfemale_deseq.tsv', sep=''), sep='\t', quote=FALSE, row.names=FALSE)
 
-fbmale <- DESeqDataSetFromMatrix(countData=counts[ , 5:8], colData=samples[5:8, ], design = ~AlignsTo)
+# Male Fat Bodies
+
+fbmale <- DESeqDataSetFromMatrix(countData=counts[ , 5:8], colData=samples[5:8, ], design = ~Replicate + AlignsTo)
 fbmale <- DESeq(fbmale)
-res <- results(fbmale)
+res <- results(fbmale, contrast=c('AlignsTo', 'ref', 'alt'))
 res$gene <- rownames(res)
 res <- res[, c(7,1,2,3,4,5,6)]
 write.table(res, paste(dirname(args[1]), '/combined/fbmale_deseq.tsv', sep=''), sep='\t', quote=FALSE, row.names=FALSE)
 
-oefemale <- DESeqDataSetFromMatrix(countData=counts[ , 9:12], colData=samples[9:12, ], design = ~AlignsTo)
+# Female Oenocytes
+
+oefemale <- DESeqDataSetFromMatrix(countData=counts[ , 9:12], colData=samples[9:12, ], design = ~Replicate + AlignsTo)
 oefemale <- DESeq(oefemale)
-res <- results(oefemale)
+res <- results(oefemale, contrast=c('AlignsTo', 'ref', 'alt'))
 res$gene <- rownames(res)
 res <- res[, c(7,1,2,3,4,5,6)]
 write.table(res, paste(dirname(args[1]), '/combined/oefemale_deseq.tsv', sep=''), sep='\t', quote=FALSE, row.names=FALSE)
 
-oemale <- DESeqDataSetFromMatrix(countData=counts[ , 13:16], colData=samples[13:16, ], design = ~AlignsTo)
+# Male oenocytes
+
+oemale <- DESeqDataSetFromMatrix(countData=counts[ , 13:16], colData=samples[13:16, ], design = ~Replicate + AlignsTo)
 oemale <- DESeq(oemale)
-res <- results(oemale)
+res <- results(oemale, contrast=c('AlignsTo', 'ref', 'alt'))
 res$gene <- rownames(res)
 res <- res[, c(7,1,2,3,4,5,6)]
 write.table(res, paste(dirname(args[1]), '/combined/oemale_deseq.tsv', sep=''), sep='\t', quote=FALSE, row.names=FALSE)
